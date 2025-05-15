08a07645ac73cb757b0c1cff3d1c329215583b81
lacddt <lacddt@lac-archivematica-live2.is.ed.ac.uk>
2025-05-15 15:27:51 +0100

    Fixes storage service CSRF nonsense

--- src/archivematica/storage_service/storage_service/settings/local.py
+++ src/archivematica/storage_service/storage_service/settings/local.py
@@ -1,7 +1,6 @@
 """Development settings and globals."""
 
 from os import environ
-
 import dj_database_url
 
 from archivematica.storage_service.common.helpers import get_env_variable
@@ -70,3 +69,6 @@ if LDAP_AUTHENTICATION and DEBUG:
     # Don't validate certs if debug is on
     AUTH_LDAP_GLOBAL_OPTIONS[ldap.OPT_X_TLS_REQUIRE_CERT] = ldap.OPT_X_TLS_NEVER
 ######### END LDAP CONFIGURATION #########
+CSRF_TRUSTED_ORIGINS = ["https://dp-storageservice.is.ed.ac.uk"]
+
+
